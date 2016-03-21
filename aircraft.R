92dd247aa8ad2af35aeb8b97c406833afcbd19aa
Konstantin Sorokin <kvs@sigterm.ru>
2016-03-21 09:08:39 +0300

    more data cleanup and descriptive statistics

--- aircraft.R
+++ aircraft.R
@@ -11,7 +11,7 @@ tables <- url %>%
 data <- tables[[1]]
 
 # fix names
-names(data) <- c("Deaths.T", "Deaths.C", "Deaths.P", "Deaths.G", "Deaths.N", "T",
+names(data) <- c("Deaths.T", "Deaths.C", "Deaths.P", "Deaths.G", "Deaths.N", "Type",
               "Incident", "Aircraft", "Location", "Phase", "Airport", "Distance", "Date")
 
 # delete the first row
@@ -34,15 +34,32 @@ empty_as_na <- function(x) {
     return(ifelse(as.character(x) != "", x, NA))
 }
 
-# cleanup data
+extract_integer <- function(x) {
+    x <- gsub("\n.*$", "", x)
+    return(as.integer(sub(".*?([0-9]+).*", "\\1", x, perl=TRUE)))
+}
+
+# cleanup and massage data
 data <- data %>%
     mutate(Phase = gsub("\n.*$", "", Phase)) %>%
     mutate(Phase = gsub("[0-9]+", "", Phase)) %>%
     mutate(Phase = gsub("\\[\\]", "", Phase)) %>%
+    mutate(Deaths.G = as.vector(sapply(Deaths.G, extract_integer))) %>%
+    mutate(Deaths.T = as.vector(sapply(Deaths.T, extract_integer))) %>%
     mutate_each(funs(empty_as_na)) %>%
+    mutate(Type = as.factor(Type),
+           Phase = as.factor(Phase)) %>%
+    mutate(Deaths.C = as.integer(Deaths.C),
+           Deaths.P = as.integer(Deaths.P)) %>%
     mutate(Country = make_countries(Location))
 
 accidents_per_country <- data %>%
     group_by(Country) %>%
-    summarise(total = n()) %>%
-    arrange(desc(total))
+    summarise(accidents = n(), passengers = sum(Deaths.P), all = sum(Deaths.T)) %>%
+    arrange(desc(accidents))
+
+accidents_per_country_com <- data %>%
+    filter(Type == "COM") %>%
+    group_by(Country) %>%
+    summarise(accidents = n(), passengers = sum(Deaths.P), all = sum(Deaths.T)) %>%
+    arrange(desc(accidents))
